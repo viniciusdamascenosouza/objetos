eac0354a87275a20340b4f4e45a6999dd0c13fa8
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-09 08:40:45 -0300

    Operações com json

--- leitura.js
+++ leitura.js
@@ -1,5 +1,14 @@
 const dados = require("./cliente.json");
 
-console.log(dados);
-console.log(typeof dados);
+// console.log(dados);
+// console.log(typeof dados);
 // O arquivo json é um objeto e não apenas uma string.
+
+const clientesEmString = JSON.stringify(dados);
+// console.log(clientesEmString);
+
+console.log(typeof clientesEmString);
+console.log(clientesEmString.nome);
+
+const objetoCliente = JSON.parse(clientesEmString);
+console.log(objetoCliente);
