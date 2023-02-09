3778b42aed025a12732fe093b487d5c558a27467
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-09 07:23:25 -0300

    Cópia chaves

---
+++ chaves.js
@@ -0,0 +1,18 @@
+const cliente = {
+  nome: "João",
+  idade: 46,
+  email: "joaozinho@dominio.com",
+  telefone: ["90100000", "921110011"],
+};
+
+cliente.enderecos = [
+  {
+    titulo: "Minha casa",
+    rua: "Rua das Industrias",
+    numero: 337,
+    complemento: "Apt. 7",
+    bairro: "industrial",
+    apartamento: true,
+  },
+];
+
