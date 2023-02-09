6d525281b7adac5ff15da5a879f37b1c1549a2ec
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-09 19:41:59 -0300

    Encontrando dentro de uma array no json

--- encontrar.js
+++ encontrar.js
@@ -1,9 +1,11 @@
 const clientes = require("./clientes.json");
 
 function encontrar(lista, chave, valor) {
-  return lista.find((item) => item[chave] === valor);
+    return lista.find((item) => item[chave].includes(valor));
 }
 
-const encontrado = encontrar(clientes, "nome", "Husein");
+const encontrado = encontrar(clientes, "nome", "Kirby");
 
-console.log(encontrado);
+const encontrado2 = encontrar(clientes, "telefone", "75993785074");
+
+console.log(encontrado2);
