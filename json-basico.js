6fface01ec70599c68fb2656276639427952e6c6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-09 08:24:50 -0300

    Formato JSON

---
+++ json-basico.js
@@ -0,0 +1,32 @@
+//------J S O N = JAVA SCRIPT OBJECT NOTATION = NOTAÇÃO DE OBJETO JAVASCRIPT-------
+
+
+/*
+{
+  "nome": "João",
+  "idade": 46,
+  "email": "joaozinho@dominio.com",
+  "telefone": [
+    "90100000",
+    "921110011"
+  ],
+  "endereco": {
+    "rua": "Rua das Industrias",
+    "numero": "337",
+    "complemento": "Apt. 7",
+    "bairro": "industrial",
+    "apartamento": true,
+    "casa": false
+  }
+}
+*/
+
+// -----DIFERENCIAIS:-----
+
+// NÃO TEM VARIAVEIS;
+// CHAVES DAS PROPRIEDADES TÊM  "";
+// ARRAYS E OBJETOS ANINHADOS PODEM EXISTIR;
+// PROIBIDO TER TRAILING COMMA (VIRGULA NO ULTIMO ITEM);
+// FUNÇÕES NÃO SÃO PERMITIDAS;
+// COMENTÁRIOS NÃO SÃO PERMITIDOS;
+// SUPORTA APENAS TIPOS PRIMITIVOS(strings, numbers, booleans e null). 
